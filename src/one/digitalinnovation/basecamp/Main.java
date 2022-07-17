9e9484c868d86b1725a071cc2db724dde5b433e1
AnneCleres28 <annecleres.ac@gmail.com>
2022-07-17 16:10:34 -0300

    first commit

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,46 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn$4678_md = 10;
+        //int asrn2$46%78_md = 10;
+
+        asrn24678md = 100;
+        //asrn2$4678_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMER_TENTAIVAS = 5;
+        //final int numeroTentantivas = 5;
+        int QUNATIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn$4678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMER_TENTAIVAS);
+        System.out.println(QUNATIDADE_OPCOES);
+
+    }
+}
